4deaf0e6b7ab846843f97ee66c6ab1dda4800609
Karolis Uziela <karolis.uziela@scilifelab.se>
2018-04-23 09:34:43 +0200

    updated ProQ3D-S and ProQ2D-S model weights with the ones trained with keras2

--- run_test.sh
+++ run_test.sh
@@ -28,9 +28,7 @@ if [[ ! -f test.$num/1e12A_0001.pdb.proq3.sscore.local || ! -f test.$num/1e12A_0
     exit 1
 else
     check1=`$R_SCRIPT ./ProQ3_scripts/test_verify.R test.$num/1e12A_0001.pdb.proq3.sscore.local tests_clean/1e12A_0001.pdb.proq3.sscore.local test.$num/1e12A_0001.pdb.proq3.sscore.global tests_clean/1e12A_0001.pdb.proq3.sscore.global 2>&1`
-    check2=`$R_SCRIPT ./ProQ3_scripts/test_verify.R test.$num/1e12A_0001.pdb.proq3.sscore.local tests_clean/1e12A_0001.pdb.proq3.sscore.local test.$num/1e12A_0001.pdb.proq3.sscore.global tests_clean/1e12A_0001.pdb.proq3.sscore.global 2>&1`
-    check3=`$R_SCRIPT ./ProQ3_scripts/test_verify.R test.$num/1e12A_0001.subset.pdb.proq3.sscore.local tests_clean/1e12A_0001.subset.pdb.proq3.sscore.local test.$num/1e12A_0001.subset.pdb.proq3.sscore.global tests_clean/1e12A_0001.subset.pdb.proq3.sscore.global 2>&1`
-    check4=`$R_SCRIPT ./ProQ3_scripts/test_verify.R test.$num/1e12A_0001.subset.pdb.proq3.sscore.local tests_clean/1e12A_0001.subset.pdb.proq3.sscore.local test.$num/1e12A_0001.subset.pdb.proq3.sscore.global tests_clean/1e12A_0001.subset.pdb.proq3.sscore.global 2>&1`
+    check2=`$R_SCRIPT ./ProQ3_scripts/test_verify.R test.$num/1e12A_0001.subset.pdb.proq3.sscore.local tests_clean/1e12A_0001.subset.pdb.proq3.sscore.local test.$num/1e12A_0001.subset.pdb.proq3.sscore.global tests_clean/1e12A_0001.subset.pdb.proq3.sscore.global 2>&1`
 fi
 
 if [[ $DEEP_INSTALLED == "yes" ]] ; then
@@ -44,16 +42,14 @@ if [[ $DEEP_INSTALLED == "yes" ]] ; then
         exit 1
     else
         check1d=`$R_SCRIPT ./ProQ3_scripts/test_verify.R test.$num/1e12A_0001_deep.pdb.proq3.sscore.local tests_clean/1e12A_0001_deep.pdb.proq3.sscore.local test.$num/1e12A_0001_deep.pdb.proq3.sscore.global tests_clean/1e12A_0001_deep.pdb.proq3.sscore.global 2>&1`
-        check2d=`$R_SCRIPT ./ProQ3_scripts/test_verify.R test.$num/1e12A_0001_deep.pdb.proq3.sscore.local tests_clean/1e12A_0001_deep.pdb.proq3.sscore.local test.$num/1e12A_0001_deep.pdb.proq3.sscore.global tests_clean/1e12A_0001_deep.pdb.proq3.sscore.global 2>&1`
-        check3d=`$R_SCRIPT ./ProQ3_scripts/test_verify.R test.$num/1e12A_0001_deep.subset.pdb.proq3.sscore.local tests_clean/1e12A_0001_deep.subset.pdb.proq3.sscore.local test.$num/1e12A_0001_deep.subset.pdb.proq3.sscore.global tests_clean/1e12A_0001_deep.subset.pdb.proq3.sscore.global 2>&1`
-        check4d=`$R_SCRIPT ./ProQ3_scripts/test_verify.R test.$num/1e12A_0001_deep.subset.pdb.proq3.sscore.local tests_clean/1e12A_0001_deep.subset.pdb.proq3.sscore.local test.$num/1e12A_0001_deep.subset.pdb.proq3.sscore.global tests_clean/1e12A_0001_deep.subset.pdb.proq3.sscore.global 2>&1`
+        check2d=`$R_SCRIPT ./ProQ3_scripts/test_verify.R test.$num/1e12A_0001_deep.subset.pdb.proq3.sscore.local tests_clean/1e12A_0001_deep.subset.pdb.proq3.sscore.local test.$num/1e12A_0001_deep.subset.pdb.proq3.sscore.global tests_clean/1e12A_0001_deep.subset.pdb.proq3.sscore.global 2>&1`
     fi
 
 fi
 
 echo "------------- Results of test run ------------------"
 
-if [[ $check1 != "" || $check2 != "" || $check3 != "" || $check4 != "" ]] ; then
+if [[ $check1 != "" || $check2 != "" ]] ; then
     echo "ProQ3 (SVM version) completed successfully, but the results in the output files don't match the sample results in tests_clean directory. Check your installation carefully..."
 else
     echo "================ Congrats! Test run for ProQ3 (SVM version) has passed without any problems! ===================="
@@ -61,7 +57,7 @@ fi
 
 
 if [[ $DEEP_INSTALLED == "yes" ]] ; then
-    if [[ $check1d != "" || $check2d != "" || $check3d != "" || $check4d != "" ]] ; then
+    if [[ $check1d != "" || $check2d != "" ]] ; then
         echo "ProQ3D (Deep Learning version) completed successfully, but the results in the output files don't match the sample results in tests_clean directory. Check your installation carefully..."
     else
         echo "================ Congrats! Test run for ProQ3D (Deep Learning version) has passed without any problems! ===================="
